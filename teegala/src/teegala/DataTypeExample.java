7a89514e4dbfa72f81e95234d0b075aba3cd5e90
AKHILA <AKHILA@DESKTOP-J6670JL>
2025-09-12 12:46:41 +0530

    oijgiorjg

---
+++ teegala/src/teegala/DataTypeExample.java
@@ -0,0 +1,14 @@
+package teegala;
+
+public class DataTypeExample {
+public static void main(String args[]) {
+	int number = 10;
+	double price = 99.99;
+	char grade = 'A';
+	boolean status = true;
+	System.out.println("Integer: " + number);
+	System.out.println("Double: " + price);
+	System.out.println("Char: " + grade);
+	System.out.println("Boolean: " + status);
+}
+}
