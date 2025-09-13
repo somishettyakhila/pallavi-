54fe8bc98a28c8047f15939f461e80aa086550cf
AKHILA <AKHILA@DESKTOP-J6670JL>
2025-09-13 12:10:55 +0530

    Initial Commit

---
+++ teegala/src/examples/operators.java
@@ -0,0 +1,22 @@
+package examples;
+
+public class operators {
+	
+	    public static void main(String[] args) {
+	       
+	        int a = 10, b = 5;
+	        System.out.println("Arithmetic Operators:");
+	        System.out.println("Addition: " + (a + b)); 
+	        System.out.println("Subtraction: " + (a - b));
+	        System.out.println("Multiplication: " + (a * b)); 
+	        System.out.println("Division: " + (a / b)); 
+	        System.out.println("Modulus: " + (a % b)); 
+
+	       
+
+	       
+	    }
+	}
+
+
+
