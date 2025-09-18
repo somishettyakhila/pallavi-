f4f31f8921252e707b1920a14262af170b446e4a
AKHILA <AKHILA@DESKTOP-J6670JL>
2025-09-18 23:55:24 +0530

    PhoneExample

---
+++ teegala/src/com/tnsif/task6/Main.java
@@ -0,0 +1,13 @@
+package com.tnsif.task6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		vivo v = new vivo();
+		v.call();
+		v.sms();
+		Oneplus s = new Oneplus();
+		s.call();
+		s.sms();
+	}
+}
---
+++ teegala/src/com/tnsif/task6/Oneplus.java
@@ -0,0 +1,11 @@
+package com.tnsif.task6;
+
+
+public class Oneplus implements PhoneEx{
+	public void call() {
+		System.out.println("samsung call is accepted");
+	}
+	public void sms() {
+		System.out.println("samsung sms is accepted");
+	}
+}
